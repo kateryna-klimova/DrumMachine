060382b1d1e837318e8d8b0756ffe342bcc2386c
kateryna-klimova <katrynaklimova@gmail.com>
2019-03-12 08:03:38 +0200

    indent 6 with beautify

--- main_script.py
+++ main_script.py
@@ -33,26 +33,31 @@ ADAFRUIT_IO_KEY = "e2eee5f63ba54e26b3fb8bb70dd4bd09"
 # Here comes your function definitions
 
 # Subscribes to the Adafruit stream
+
+
 def connected(client):
     client.subscribe('google_commands')
 
 # this gets called every time a message is received
-def message(client, feed_id, payload): #proces the commands from Google Home
+
+
+def message(client, feed_id, payload):  # proces the commands from Google Home
      global drum_pad_mode
      if payload == "mode piano":
         drum_pad_mode = 'piano'
-        print ("Mode set to " + drum_pad_mode)
+        print("Mode set to " + drum_pad_mode)
      elif payload == "mode drums":
         drum_pad_mode = 'drums'
-        print ("Mode set to " + drum_pad_mode)
+        print("Mode set to " + drum_pad_mode)
      elif payload == "record":
         record_tag = 1
-        print ("Recording started")
+        print("Recording started")
      elif payload == "stop recording":
         record_tag = 0
-        print ("Recording stopped")
+        print("Recording stopped")
      else:
         print "Message from Google Home: %s" % payload
+
 
 def callback1(channel):
     # if drum_pad_mode == 'default':
@@ -77,6 +82,7 @@ def callback2(channel):
     #     print "Playing from port 23 in mode %s" % drum_pad_mode
     b2 = 1
 
+
 def callback3(channel):
     # if drum_pad_mode == 'default':
     #     print "Playing from port 18 in mode %s" % drum_pad_mode
@@ -87,7 +93,6 @@ def callback3(channel):
     #     os.system('aplay -D bluealsa ~/Desktop/project/drums/hat.wav')
     #     print "Playing from port 18 in mode %s" % drum_pad_mode
     b3 = 1
-
 
 
 # Function defenitions end hello
@@ -104,45 +109,46 @@ def main():
     global b9
     global record_tag
     client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-    client.on_connect    = connected
-    client.on_message    = message
+    client.on_connect = connected
+    client.on_message = message
     client.connect()
     GPIO.add_event_detect(24, GPIO.BOTH, callback=callback1, bouncetime=500)
     GPIO.add_event_detect(18, GPIO.BOTH, callback=callback2, bouncetime=500)
     GPIO.add_event_detect(23, GPIO.BOTH, callback=callback3, bouncetime=500)
     while True:
-            if drum_pad_mode == 'piano':
-                if b1 == 1:
+            if (drum_pad_mode == 'piano'):
+                if (b1 == 1):
                         print "Playing b1 in mode %s" % drum_pad_mode
                         b1 = 0
-                if b2 == 1:
+                if (b2 == 1):
                         print "Playing b2 in mode %s" % drum_pad_mode
                         b2 = 0
-                if b3 == 1:
+                if (b3 == 1):
                         print "Playing b3 in mode %s" % drum_pad_mode
                         b3 = 0
-            elif drum_pad_mode == 'drums':
-        	    if b1 == 1:
+            elif (drum_pad_mode == 'drums'):
+        	    if (b1 == 1):
                         print "Playing b1 in mode %s" % drum_pad_mode
                         b1 = 0
-                if b2 == 1:
+                if (b2 == 1):
                         print "Playing b2 in mode %s" % drum_pad_mode
                         b2 = 0
-                if b3 == 1:
+                if (b3 == 1):
                         print "Playing b3 in mode %s" % drum_pad_mode
                         b3 = 0
             else:
-                if b1 == 1:
+                if (b1 == 1):
                         print "Playing b1 in mode %s" % drum_pad_mode
                         b1 = 0
-                if b2 == 1:
+                if (b2 == 1):
                         print "Playing b2 in mode %s" % drum_pad_mode
                         b2 = 0
-                if b3 == 1:
+                if (b3 == 1):
                         print "Playing b3 in mode %s" % drum_pad_mode
                         b3 = 0
     client.loop_blocking()
     GPIO.cleanup()
+
 
 if __name__ == "__main__":
         main()
