567a77f685950d785d2d4073bb76e0538a76e17a
kateryna-klimova <katrynaklimova@gmail.com>
2019-03-13 05:15:43 +0200

    fvd

--- main_script.py
+++ main_script.py
@@ -123,10 +123,10 @@ def main():
     global b9
     global record_tag
     global drum_pad_mode
-    # client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
-    # client.on_connect = connected
-    # client.on_message = message
-    # client.connect()
+    client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+    client.on_connect = connected
+    client.on_message = message
+    client.connect()
     GPIO.add_event_detect(24, GPIO.BOTH, callback=callback1, bouncetime=500)
     GPIO.add_event_detect(18, GPIO.BOTH, callback=callback2, bouncetime=500)
     GPIO.add_event_detect(23, GPIO.BOTH, callback=callback3, bouncetime=500)
