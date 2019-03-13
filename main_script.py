4aa6972dc7874e5ddb47f0243bed71d46862fc28
kateryna-klimova <katrynaklimova@gmail.com>
2019-03-13 05:17:02 +0200

    vcb

--- main_script.py
+++ main_script.py
@@ -131,10 +131,6 @@ def main():
     GPIO.add_event_detect(18, GPIO.BOTH, callback=callback2, bouncetime=500)
     GPIO.add_event_detect(23, GPIO.BOTH, callback=callback3, bouncetime=500)
     while True:
-        sleep(0.5)
-        print(b1)
-        print(b2)
-        print(b3)
         if (drum_pad_mode == 'piano'):
             if (b1 == 1):
                 print "Playing b1 in mode %s" % drum_pad_mode
